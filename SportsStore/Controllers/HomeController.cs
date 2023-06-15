02a166d1deb6fe076d37b0ddbb148057d48831e5
RuralYoung <ruralyoungiii@gmail.com>
2023-06-15 16:04:03 -0400

    Modfied the HomeController and Index to reflect the changes made to the the ViewModel

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
-    public class HomeController: Controller
+    public class HomeController : Controller
     {
         private IStoreRepository repository;
         public int PageSize = 4;
@@ -12,10 +13,19 @@ namespace SportsStore.Controllers
         {
             repository = repo;
         }
-        public ViewResult Index(int productPage = 1)
-            => View(repository.Products
-                .OrderBy(p => p.ProductID)
-                .Skip((productPage - 1) * PageSize)
-                .Take(PageSize));
+        public ViewResult Index(int productPage = 1) 
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                        .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
